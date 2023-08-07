00794d2349837cc32e61f7cd2b0861215e8a8f92
Oluwashina Lasisi <lasisioluwashina@gmail.com>
2023-08-07 23:32:00 +0100

    Feat: Added cart screen to the app

--- src/Screens/CartScreen.js
+++ src/Screens/CartScreen.js
@@ -1,11 +1,18 @@
-import { Text, View } from 'native-base'
+import { Text, View, Box, Center } from 'native-base'
 import React from 'react'
+import CartEmpty from '../Components/CartEmpty'
+import Colors from '../colors'
 
 function CartScreen() {
   return (
-  <View>
-    <Text>Cart Screen</Text>
-  </View>
+  <Box flex={1} safeAreaTop bg={Colors.white}>
+  {/* Header */}
+  <Center w="full" py={5}>
+    <Text color={Colors.black} fontSize={20} bold>Cart</Text>
+    </Center>
+    {/* If Cart is Empty */}
+    <CartEmpty/>
+  </Box>
   
   )
 }
