78c8c75ffd9f5ad0ca02f053a8f715eb7548f433
Moses Oluwashina  Lasisi <lasisioluwashina@gmail.com>
2023-08-07 23:40:26 +0100

    Update CartScreen.js

--- src/Screens/CartScreen.js
+++ src/Screens/CartScreen.js
@@ -10,7 +10,7 @@ function CartScreen() {
   <Center w="full" py={5}>
     <Text color={Colors.black} fontSize={20} bold>Cart</Text>
     </Center>
-    {/* If Cart is Empty */}
+    {/*If Cart is Empty*/}
     <CartEmpty/>
   </Box>
   
