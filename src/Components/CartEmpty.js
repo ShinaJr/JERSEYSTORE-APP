48eb8a51c81e507b25f0f7bc483382362279a204
Oluwashina Lasisi <lasisioluwashina@gmail.com>
2023-08-07 23:35:14 +0100

    feat: added a cartempty screen

---
+++ src/Components/CartEmpty.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const CartEmpty = () => {
+  return (
+    <View>
+      <Text>CartEmpty</Text>
+    </View>
+  )
+}
+
+export default CartEmpty
+
